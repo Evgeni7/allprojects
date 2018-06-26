53cec3a7e2ef1b9d7f15fea22426a19c263137da
Evgeni7 <40259589+Evgeni7@users.noreply.github.com>
2018-06-26 17:53:23 +0300

    Visualization

---
+++ EKATTE/index.php
@@ -0,0 +1,98 @@
+<html>
+<head>
+
+</head>
+<body>
+
+<form method="post">
+<input type="text" name="entry">
+
+<input type="submit">
+</form>
+
+<?php
+$link = mysqli_connect('localhost', 'wordpressuser', 'Passw0rd!', 'ekatte');
+if (!$link) 
+{
+    $output = 'Unable to connect.';
+	echo $output;	
+	exit();
+}
+#$select = $_POST['search'];
+$entry = $_POST['entry'];
+if ($entry == null)
+{
+	$maxid = "select * from sel inner join obst join obl where sel.obstina = obst.obstina and obst.oblast = obl.oblast";
+	$result = $link->query($maxid);
+	$row = $result->fetch_assoc();
+}
+else
+{
+	$maxid = "select * from sel inner join obst join obl where sel.obstina = obst.obstina and obst.oblast = obl.oblast and sel.name = '$entry'";
+	$result = $link->query($maxid);
+	$row = $result->fetch_assoc();
+}
+
+#elseif ($select=="select1")
+#{
+#	$maxid = "SELECT * FROM sel where name = '$entry'";
+#	$result = $link->query($maxid);
+#	$row = $result->fetch_assoc();
+#	echo "Селище '$entry'";
+#}
+
+#elseif ($select=="select2")
+#{
+#	$maxid = "SELECT * FROM sel where obstina = (select obstina from sel where name = '$entry')";
+#	$result = $link->query($maxid);
+#	$row = $result->fetch_assoc();
+#	echo "Селища в община '$entry'";
+#}
+#
+#elseif ($select=="select3")
+#{
+#	$maxid = "SELECT * from sel where obstina = (select obstina from sel where name = '$entry')";
+#	$result = $link->query($maxid);
+#	$row = $result->fetch_assoc();
+#	echo "Селища в област '$entry'";
+#	echo $result['name'];
+#}
+
+
+#<select name="search">
+#	<option value="select1">Селища</option>
+#	<option value="select2">Община</option>
+#	<option value="select3">Област</option>
+#</select> 
+
+
+
+
+?>
+<div name="result" style="width: 100%; height:80%; top 20%; ">
+<?php
+foreach ($result as $result)
+{
+	echo $result['num'];
+	echo " ";
+	echo $result['tvm'];
+	echo " ";
+	echo $result['name'];
+	echo " ";
+	echo $result['oblast'];
+	echo " ";
+	echo $result['obstina'];
+	echo " ";
+
+
+?>
+<br>
+<?php
+}
+?>
+</div>
+
+
+
+</body>
+</html>
