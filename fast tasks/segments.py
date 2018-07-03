ea548b1d89efed98ab2db3a7e7995be2a3c35f59
Evgeni7 <40259589+Evgeni7@users.noreply.github.com>
2018-07-03 14:14:38 +0300

    Update segments.py

--- fast tasks/segments.py
+++ fast tasks/segments.py
@@ -12,7 +12,6 @@ try:
         counter = t + int(a)
         AY[counter]=1
         t = counter
-        print counter
         
 except:
     pass
@@ -26,31 +25,36 @@ try:
         if counter > 0:
             AY[counter] = 1
             t = counter
-            print counter
 except:
     pass
-for i in range(int(n)):
-    print AY[i]
-    i = i + 1
+
 
 j = 0
 jk = 0
 
-#for j in xrange(int(n)):
- #   if AY[j] == 1:
-  #      jj = j + int(c)
-   #     if jj <= int(n):
-    #        if AY[jj] == 1:
-     #           for i in range (j, jj):
-      #              AY[i] = 2
-       #             i = i + 1
-
 for j in range(int(n)):
-    if AY[j] == 1:
+    if AY[j] == 1 or AY[j] == 3:
         try:
-            if AY[j+int(c)] == 1:
-                jk = jk + 1
+            if AY[j+int(c)] == 1 or AY[j+int(c)] == 3:
+                i = j
+                for i in range(j-1, j + int(c)):
+                    i = i + 1
+                    if AY[i] == 1 or AY[i] == 3:
+                        AY[i] = 3
+                    else:
+                        AY[i] = 2
         except:
             pass
                 
-print(int(n) - jk)
+
+for i in range(int(n)):
+    print AY[i]
+    i = i + 1
+
+
+co = 0
+for jk in range(int(n)):
+    if AY[jk]==0 or AY[jk]==1:
+        co = co + 1
+print "Answer is: "
+print co
